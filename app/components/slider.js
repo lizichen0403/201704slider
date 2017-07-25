4a8c1f7332a0a8d8bae3f9305f138c28787c9b95
lizichen0403 <lizichen90@vip.qq.com>
2017-07-25 15:42:11 +0800

    轮播图

--- app/components/slider.js
+++ app/components/slider.js
@@ -9,10 +9,17 @@ export default class Slider extends Component{
     //移动index
     turn=(step)=>{
         let index=this.state.index+step;
-        if(index>=this.props.images.length){
-            index=0;
+        if(index>this.props.images.length){
+            this.sliders.style.transitionDuration='0s';
+            this.sliders.style.left=0;
+            index=1;
+            setTimeout(()=>{
+                this.sliders.style.transitionDuration='1s';
+                this.setState({index});
+            },20);
+            return;
         }
-        this.setState({index})
+        this.setState({index});
     }
     //开始自动轮播,鼠标经过时暂停自动轮播,移走后开启轮播
     go=()=>{
@@ -21,6 +28,7 @@ export default class Slider extends Component{
         },2000);//每隔两秒钟让index＋1
     }
     componentDidMount(){
+        this.sliders=document.querySelector('.sliders');
         this.go();
     }
     render(){
@@ -36,4 +44,3 @@ export default class Slider extends Component{
         )
     }
 }
-
